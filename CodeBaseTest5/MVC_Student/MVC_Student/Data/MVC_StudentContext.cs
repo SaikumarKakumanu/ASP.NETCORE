37a63e40eb55fdba8fc26e20122413a69d3565cb
Saikumar Kakumanu <s.kakumanu@zensar.com>
2022-06-27 17:55:14 +0530

    Codebase test 5

---
+++ CodeBaseTest5/MVC_Student/MVC_Student/Data/MVC_StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Student.Models;
+
+namespace MVC_Student.Data
+{
+    public class MVC_StudentContext : DbContext
+    {
+        public MVC_StudentContext (DbContextOptions<MVC_StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Student.Models.Student>? Student { get; set; }
+    }
+}
