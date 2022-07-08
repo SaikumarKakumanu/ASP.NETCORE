5d99805bf8a8c7f963d15c371d689f39852cddd1
Saikumar Kakumanu <s.kakumanu@zensar.com>
2022-07-08 18:06:16 +0530

    Final project

---
+++ FinalProject/CoreMiniProject/CoreMiniProject/Data/CoreMiniProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoreMiniProject.Models;
+
+namespace CoreMiniProject.Data
+{
+    public class CoreMiniProjectContext : DbContext
+    {
+        public CoreMiniProjectContext (DbContextOptions<CoreMiniProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoreMiniProject.Models.Product> Product { get; set; }
+    }
+}
