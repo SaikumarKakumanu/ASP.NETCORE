5645ee5c1e1cf2b402b8e78d2883cd08c5b3d548
Saikumar Kakumanu <s.kakumanu@zensar.com>
2022-07-11 10:46:09 +0530

    Codebase Test(Jquey and Ajax)

---
+++ CodeBaseEvaluation(Jquery&Ajax)/ClinetWebApplication/ClinetWebApplication/Data/ClinetWebApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ClinetWebApplication.Models;
+
+namespace ClinetWebApplication.Data
+{
+    public class ClinetWebApplicationContext : DbContext
+    {
+        public ClinetWebApplicationContext (DbContextOptions<ClinetWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ClinetWebApplication.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ CodeBaseEvaluation(Jquery&Ajax)/ClinetWebApplication/ClinetWebApplication/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinetWebApplication.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+
+        [Required(ErrorMessage ="First Name is Mandatory")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string Address { get; set; }
+    }
+}
